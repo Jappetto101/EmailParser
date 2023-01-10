d62f5485e712a6434608dd36586b7d1c37d112bf
Jappetto101 <109493645+Jappetto101@users.noreply.github.com>
2023-01-10 11:45:55 -0700

    Added comments

--- emailParser.py
+++ emailParser.py
@@ -1,20 +1,28 @@
-# import openpyxl
-
+# pandas and openpyxl libraries required in order to run
 import pandas as pd
 
+# txt file should be placed in the root folder where the emailParser.py
+# file is located. It should be named "emailoutput.txt"
 filename = "emailoutput.txt"
 
 contents = open(filename,'r').read().split('\n')
 
+# These are the columns being output to an excel file
 eDict = {'msgID':[], 'From':[],'Sent':[], 'To':[], 'Cc':[], 'Subject':[], 'Attachments':[], 'Body':[]}
 
 i=0
 msgCount = 0
 body = ""
+
+# Looping through each line of the txt file to figure out where to put it
 while i < len(contents):
+    # removing all empty lines
     if contents[i].strip() == "":
         i+=1
         continue
+    # The "From:" header initializes a new message ID and checks for
+    # other header information like "Sent", "To", "Cc", "Attachments".
+    # If no header information is found, an empty string is inserted.
     elif contents[i].strip()[0:5]=="From:":
         eDict['msgID'].append(msgCount+1)
         eDict['Body'].append("")
@@ -53,11 +61,13 @@ while i < len(contents):
         msgCount += 1
         body = ""
         continue
+    # Once header information is found, the body of the message is collated
     elif contents[i].strip()[0:5] != "From:":
         body += contents[i]
         body += "\n"
         i+=1
         continue
-
+    
+# Outputs result of code into xlsx
 df=pd.DataFrame(eDict)
 df.to_excel('output.xlsx', index= 0)
